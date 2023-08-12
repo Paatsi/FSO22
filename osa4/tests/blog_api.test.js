62be8f7fb5506bc234fa58010c9f48a3c13d8073
Paatsi <paatsidev@gmail.com>
2023-08-12 13:49:22 +0300

    exercise 4.8

---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('tests for initial blogs in the DB', () => {
+  test('correct amount of blogs are returned, 2', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('check that blogs are in JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
