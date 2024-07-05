e820d85cbba6c8c657ba75d2c8b7ac685da6bfcf
Paatsi <paatsidev@gmail.com>
2024-07-05 18:49:58 +0300

    exercise 5.15

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -55,3 +55,36 @@ test('clicking the button reveals rest of the information', async () => {
   const likesElement = screen.getByText('likes 1')
   const userElement = screen.getByText('Testaaja')
 })
+
+test('clicking the like button twice triggers the handler function twice', async () => {
+  const blog = {
+    title: 'Vitest author',
+    author: 'Tester',
+    url: 'www.vitest.com',
+    likes: 1,
+    user: {
+      name: 'Testaaja',
+      id:'öööö'
+    }
+  }
+
+  const user = {
+    name: 'Testaaja',
+    id: 'öööö'
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} user={user} toggleVisibility={mockHandler} addLikes={mockHandler} />)
+
+  const viUser = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await viUser.click(viewButton)
+  expect(viewButton).toBeDefined()
+
+  const likeButton = screen.getByText('like')
+  expect(likeButton).toBeDefined()
+  await viUser.click(likeButton)
+  await viUser.click(likeButton)
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
