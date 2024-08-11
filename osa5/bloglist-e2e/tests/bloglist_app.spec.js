738bfda56aea6654847e9e2c69a13c81dc24dfa6
Paatsi <paatsidev@gmail.com>
2024-08-11 15:33:47 +0300

    exercise 5.17

---
+++ osa5/bloglist-e2e/tests/bloglist_app.spec.js
@@ -0,0 +1,24 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Tester',
+        username: "Test User",
+        password: "password123"
+      }
+    })
+
+    await page.goto('/')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+
+  })
+})
