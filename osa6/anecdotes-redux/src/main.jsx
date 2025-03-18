5a7509a4a536415116b5e0724aeefb3806c17296
Paatsi <paatsidev@gmail.com>
2025-03-18 19:42:24 +0200

    exercise 6.14 & 6.15

--- osa6/anecdotes-redux/src/main.jsx
+++ osa6/anecdotes-redux/src/main.jsx
@@ -1,7 +1,14 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
 import App from './App'
 import store from './store'
+
+
+anecdoteService.getAll().then(anecdotes =>
+	store.dispatch(setAnecdotes(anecdotes))
+  )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
