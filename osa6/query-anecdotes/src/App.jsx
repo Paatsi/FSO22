5eb7bcc58d973ea82ae288485b05c642d0bed486
Paatsi <paatsidev@gmail.com>
2025-04-10 22:19:20 +0300

    exercise 6.22

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,51 +1,56 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
-
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log('vote')
-  }
+	const queryClient = useQueryClient()
 
-  const result = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes,
-    retry: false
-  })
-  console.log(JSON.parse(JSON.stringify(result)))
+	const handleVote = (anecdote) => {
+		updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+	}
 
-  if ( result.isLoading ) {
-    return <div>loading data...</div>
-  }
+	const updateAnecdoteMutation = useMutation({
+		mutationFn: updateAnecdote,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+		}
+	})
 
-  if ( result.isError ) {
-    return <div>anecdote service not available due to problems in server</div>
-  }
+	const result = useQuery({
+		queryKey: ['anecdotes'],
+		queryFn: getAnecdotes,
+		retry: false
+	})
 
-  const anecdotes = result.data
+	if (result.isLoading) {
+		return <div>loading data...</div>
+	}
 
-  return (
-    <div>
-      <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
-  )
+	if (result.isError) {
+		return <div>anecdote service not available due to problems in server</div>
+	}
+
+	const anecdotes = result.data
+
+	return (
+		<div>
+			<h3>Anecdote app</h3>
+
+			<Notification />
+			<AnecdoteForm />
+
+			{anecdotes.map((anecdote) => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => handleVote(anecdote)}>vote</button>
+					</div>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default App
